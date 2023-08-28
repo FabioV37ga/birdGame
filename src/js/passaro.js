ec26289a74a7b2dbf243b7e882191724bfe0efa1
Fábio Veiga <veigabfabio@hotmail.com>
2023-08-28 13:05:13 -0200

    Iniciando desenvolvimento principal

--- src/js/passaro.js
+++ src/js/passaro.js
@@ -1,5 +1,5 @@
 class Passaro {
-    static elemento = document.querySelector(".slider");
+    static elemento = document.querySelector(".passaro");
     static tamanho = 8;
     static posicaoY = 0;
     static pulando = false;
@@ -10,8 +10,8 @@ class Passaro {
         if (this.pulando == false) {
             // Define pulando como true
             this.pulando = true;
-            // Define posição final do pulo, limitando em 400 - this.tamanho
-            var posicaoFinal = (this.posicaoY + 100) >= 400 - this.tamanho ? 400 - this.tamanho : this.posicaoY + 100;
+            // Define posição final do pulo, limitando em 560 - this.tamanho
+            var posicaoFinal = (this.posicaoY + 100) >= 560 - this.tamanho ? 560 - this.tamanho : this.posicaoY + 100;
             /* 
             * Intervalo: faz a animação de pulo adicionando 2 de altura por execução até 
             * atingir o valor da variavel posicaoFinal;
@@ -22,13 +22,13 @@ class Passaro {
                 // Adiciona altura
                 this.posicaoY += 2;
                 // Chama o método para atualizar a posicao se ela ainda não atingiu posicaoFinal
-                this.posicaoY >= 394 ? null : this.atualizarPosicao();
+                this.posicaoY >= 654 ? null : this.atualizarPosicao();
                 // Finaliza o intervalo se a posicao atingiu posicaoFinal
                 if (this.posicaoY >= posicaoFinal) {
                     // Habilita queda do pássaro se estiver desabilitada
                     this.caindo == false ? this.cair() : null;
                     // Se o pássaro atinge o teto da fase ele morre
-                    this.posicaoY >= 400 - this.tamanho ? this.morrer() : null;
+                    this.posicaoY >= 560 - this.tamanho ? this.morrer() : null;
                     // Define pulando como falso
                     this.pulando = false;
                     // Limpa o intervalo
@@ -45,7 +45,7 @@ class Passaro {
         // limite 0px
         var intervalo = setInterval(() => {
             if (this.pulando == false) {
-                // Enquanto a posicao do passaro for > 0, diminui 0.5 a cada execução
+                // Enquanto a posicao do passaro for > 0, aceleradamente cai em direção ao chão da fase
                 if (this.posicaoY > 0) {
                     // Aceleração de queda: aumenta a velocidade de queda a cada execução
                     this.velocidade += 0.009
@@ -53,11 +53,11 @@ class Passaro {
                     this.posicaoY -= this.velocidade
                     // atualiza posicao a cada execução
                     this.atualizarPosicao()
-                    // Se o pássaro atingir posicao 0 (chão da fase) ele morre
                 } else {
-                    this.velocidade = 0.5
-                    // ...
+                    // Se o pássaro atingir posicao 0 (chão da fase) ele morre
                     this.morrer();
+                    // redefine velocidade de queda para 0.5;
+                    this.velocidade = 0.5
                     // define caindo como false
                     this.caindo = false;
                     // Limpa o intervalo;
