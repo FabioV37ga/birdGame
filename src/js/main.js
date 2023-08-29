b3697ba44d4864dbc055df13177590b7792c21a5
Fábio Veiga <veigabfabio@hotmail.com>
2023-08-29 20:01:09 -0200

    comentários...

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,26 @@
 var start = false;
 
+// Input SPACEBAR
 document.querySelector("body").addEventListener("keydown", function (e) {
     if (e.keyCode == 32) {
+        // Se o jogo ainda não foi iniciado, inicia.
         if (start == false) {
             start = true
             Jogo.iniciar()
         }
+        // Faz o pássaro pular
         Passaro.pular();
     }
 })
 
+// Input MOUSEDOWN
 document.querySelector("body").addEventListener("mousedown", function () {
+    // Se o jogo ainda não foi iniciado, inicia.
     if (start == false) {
         start = true
         Jogo.iniciar()
     }
+    // Faz o pássaro pular
     Passaro.pular();
 })
 
