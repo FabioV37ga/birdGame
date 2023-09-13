1975cd91832632a0200afe352ab898292ea3e8c0
Fábio Veiga <veigabfabio@hotmail.com>
2023-09-13 00:29:23 -0200

    Nova lógica p/ criar e mover os canos

--- src/js/pipe.js
+++ src/js/pipe.js
@@ -2,41 +2,59 @@ class Pipe {
     static id = 0;
     static range = [-470, -170];
     static jquery = `<div class="pipe pipemove">
-    <img src="src/img/pipes.png" alt="" srcset="">
+    <img src="src/img/pipes.png" draggable="false">
     </div>`
     id;
     elemento;
     posicaoY;
 
     constructor() {
-        Pipe.criar()
-        Pipe.atribuir();
-        Pipe.id++
+        this.criar()
+        this.atribuir();
+        this.posicionarY();
+        this.moverX();
     }
 
-    static criar() {
+    criar() {
         $(".pipes").append(Pipe.jquery)
+        this.id = Pipe.id;
+        Pipe.id++;
     }
 
     // range -470 → -170
-    static atribuir() {
-        var pipe = document.querySelectorAll(".pipe").length - 1
-        // this.id = (pipe) <= 0 ? 0 : pipe;
-        this.posicaoY = Math.floor(Math.random() * (Pipe.range[1] - Pipe.range[0]) + Pipe.range[0])
-        this.elemento = document.querySelectorAll(".pipemove")[Pipe.id]
-        this.elemento.style.top = `${this.posicaoY}px`
-        // console.log(`top: ${524 + this.posicaoY}px`)
-        // console.log(`bottom: ${524 + this.posicaoY + 111}px`)
-        document.querySelector(".pipemove").addEventListener("animationiteration", () => {
-            // this.elemento.remove()
-            // var cano = new Pipe();
-        })
-        Pipe.atualizarPosicao()
+    atribuir() {
+        var elementoHtml = document.querySelectorAll(".pipemove");
+        this.elemento = elementoHtml[elementoHtml.length - 1];
+        this.elemento.setAttribute("value", this.id)
     }
 
-    static atualizarPosicao() {
+    posicionarY() {
+        this.posicaoY = Math.floor(Math.random() * (Pipe.range[1] - Pipe.range[0]) + Pipe.range[0])
+        this.elemento.style.top = `${this.posicaoY}px`
         // console.log(this.elemento.style.top)
     }
+
+    moverX() {
+        var posicaoX = -86;
+        var intervalo = setInterval(() => {
+            posicaoX += 0.75;
+            this.elemento.style.right = `${posicaoX}px`
+            // Condicoes para finalizar o intervalo
+            // 1. Caso o cano chegue do outro lado da tela
+            posicaoX >= 483 ? clearInterval(intervalo) + this.apagar() : null;
+            // 2. Caso o jogo seja finalizado no meio da animação
+            Jogo.iniciado == true ? null : clearInterval(intervalo)
+        }, 1);
+    }
+
+    apagar(){
+        console.log(`remove \n${this.elemento}`)
+        this.elemento.remove()
+    }
+
+
+
+
 
 
 
